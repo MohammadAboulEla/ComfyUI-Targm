b39fc0ef92f129394af579746171077ad6855a8b
Makadi <rendreal@hotmail.com>
2026-03-16 01:25:27 +0200

    add settings

--- web/targm_node.js
+++ web/targm_node.js
@@ -1,13 +1,42 @@
 import { app } from "../../../scripts/app.js";
 
 const showToast = (severity, summary, detail) => {
-  app.extensionManager.toast.add({ severity, summary, detail, life: 2000 });
+  app.extensionManager.toast.add({ severity, summary, detail, life: 3000 });
 };
+
+const nodeSettings = [
+  {
+    id: "Targm.Nodes.AllowAutoColoring",
+    name: "Allow auto coloring",
+    type: "boolean",
+    defaultValue: true,
+    tooltip: "Allow auto coloring for Targm nodes when created.",
+    toastMsg: "Refresh your browser to apply changes.",
+  },
+];
+
+app.registerExtension({
+  name: "targm_settings_manager",
+  settings: nodeSettings.map((s) => ({
+    id: s.id,
+    name: s.name,
+    type: s.type,
+    defaultValue: s.defaultValue,
+    options: s.options,
+    tooltip: s.tooltip,
+    onChange: (value) => {
+      const prevValue = app.ui.settings.getSettingValue(s.id);
+      if (prevValue !== undefined && prevValue !== value) {
+        showToast("warn", "Alert!", s.toastMsg);
+      }
+    },
+  })),
+});
 
 app.registerExtension({
   name: "base.Targm",
   async nodeCreated(node) {
-    const allow_auto_coloring = false;
+    const allow_auto_coloring = app.ui.settings.getSettingValue("Targm.Nodes.AllowAutoColoring");
     if (node.comfyClass !== "TARGM") {
       return;
     }
